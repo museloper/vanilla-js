435f0189c45394ec623f8ccf629bde0ba620012a
museloper <nosf6842@naver.com>
2022-06-07 12:08:54 +0900

    2.1 ~ 2.4 Booleans

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -1,1 +1,38 @@
-alert("hi");
+// 변수 미사용
+console.log(5 + 3);
+console.log(5 - 3);
+console.log(5 * 3);
+console.log(5 / 3);
+
+console.log(4 + 2);
+console.log(4 - 2);
+console.log(4 * 2);
+console.log(4 / 2);
+
+// const
+// 상수는 값을 변경할 수 없다.
+const a = 5;
+const b = 3;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+// let
+// 변수는 값을 변경할 수 있다.
+let c = 5;
+let d = 3;
+
+console.log(c + d);
+console.log(c - d);
+console.log(c * d);
+console.log(c / d);
+
+c = 4;
+d = 2;
+
+console.log(c + d);
+console.log(c - d);
+console.log(c * d);
+console.log(c / d);
