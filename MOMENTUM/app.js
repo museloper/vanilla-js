6110d67962af6333599bd548a394730e8e2e3f4a
museloper <nosf6842@naver.com>
2022-06-15 06:43:50 +0900

    2.13 ~ 2.16 Conditionals

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -1,74 +1,8 @@
-function sayHello() {
-  console.log("Hello!");
-}
+const age = parseInt(prompt("How old are you?"));
 
-sayHello();
-
-function sayHello2(name, age) {
-  console.log("Hello my name is " + name + " and I'm " + age);
-}
-
-sayHello2("museloper", 32);
-
-function plus(x, y) {
-  console.log(x + y);
-}
-
-plus(5, 3);
-
-function divide(x, y) {
-  console.log(a / b);
-}
-
-divide(5, 3);
-
-const player = {
-  name: "museloper",
-  sayHello: function (name) {
-    console.log("Hello! " + name + " nice to meet you!");
-  },
-};
-
-console.log(player.name);
-player.sayHello("googlee");
-
-const calculator = {
-  addition: function (a, b) {
-    console.log(a + b);
-  },
-
-  subtraction: function (a, b) {
-    console.log(a - b);
-  },
-
-  multiplication: function (a, b) {
-    console.log(a * b);
-  },
-
-  division: function (a, b) {
-    console.log(a / b);
-  },
-};
-
-calculator.addition(5, 3);
-calculator.subtraction(5, 3);
-calculator.multiplication(5, 3);
-calculator.division(5, 3);
-
-const calculator2 = {
-  addition: function (a, b) {
-    return a + b;
-  },
-
-  subtraction: function (a, b) {
-    return a - b;
-  },
-
-  multiplication: function (a, b) {
-    return a * b;
-  },
-
-  division: function (a, b) {
-    return a / b;
-  },
-};
+if (isNaN(age) || age < 0) alert("Please write number");
+else if (age < 18) alert("You are too young");
+else if (age >= 18 && age <= 50) alert("You can drink");
+else if (age >= 50 && age <= 80) alert("You should exercise");
+else if (age === 100) alert("You are wise");
+else if (age > 80) alert("You can do whatever you want");
