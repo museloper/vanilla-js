17309602ef08a31cf4533a5462d98f09e85baae7
museloper <nosf6842@naver.com>
2022-06-19 12:06:54 +0900

    3.0 ~ 3.2 Searching For Elements

--- MOMENTUM/app.js
+++ MOMENTUM/app.js
@@ -1,8 +1,23 @@
-const age = parseInt(prompt("How old are you?"));
+// document
+console.log(document);
 
-if (isNaN(age) || age < 0) alert("Please write number");
-else if (age < 18) alert("You are too young");
-else if (age >= 18 && age <= 50) alert("You can drink");
-else if (age >= 50 && age <= 80) alert("You should exercise");
-else if (age === 100) alert("You are wise");
-else if (age > 80) alert("You can do whatever you want");
+// console.dir Element를 더 상세히 보여준다.
+console.dir(document);
+document.title = "Hello! From JS!";
+
+// getElementById
+const title = document.getElementById("title");
+console.log(title);
+title.innerText = "Got you!";
+
+// getElementsByClassName
+const hellos = document.getElementsByClassName("hello");
+console.log(hellos);
+
+// querySelector
+const title2 = document.querySelector(".hello span");
+console.log(title2);
+
+// querySelectorAll
+const titles = document.querySelectorAll(".hello span");
+console.log(titles);
